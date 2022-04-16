d4956ffccbc4ffed7ad4e0baaa94324477cb0508
Michele Dorph De Chiffre <md@autonik.dk>
2022-04-16 20:40:09 +0200

    Revert "Converta a usare Typescript senza moduli"
    This reverts commit 81648beee8ced7dcceba25289223aa1fbc25bf80.

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { createBasicConfig } from '@open-wc/building-rollup';
 const baseConfig = createBasicConfig();
 
 export default merge(baseConfig, {
-  input: './out-tsc/src/index.js',
+  input: './out-tsc/index.js',
   output: {
       dir: 'dist',
   }
